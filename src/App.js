89c972f9367eaf36be42ee28bb8dcb60d17c1d4c
Syed Abdullah Ali <mtuais95@gmail.com>
2020-08-25 07:57:31 +0400

    JSX ki basic exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function App() {
+  function getTitle(title) {
+    return title;
+  }
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
